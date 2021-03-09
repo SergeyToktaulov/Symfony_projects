44c2fe1c76e31ecd9eaab83b55596df0ab8e869e
SergeyToktaulov <79846955+SergeyToktaulov@users.noreply.github.com>
2021-03-10 00:37:47 +0300

    Add files via upload

---
+++ my_first_page/src/Controller/PageController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PageController extends AbstractController
+{
+
+    public function input()
+    {
+
+
+        return $this->render('mypage.html.twig'
+        );
+    }
+}
