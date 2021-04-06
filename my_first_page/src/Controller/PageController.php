8414f9957288466b0978465207936e7e904464d5
SergeyToktaulov <toktaylov15@gmail.com>
2021-04-06 15:34:07 +0300

    Added blocks with pictures
    Added blocks with pictures

--- my_first_page/src/Controller/PageController.php
+++ my_first_page/src/Controller/PageController.php
@@ -2,11 +2,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use App\Module\Hobbies;
 class PageController extends AbstractController
 {
-    public function input()
+    public function index()
     {
-        return $this->render('mypage.html.twig');
+        $hobbies = new Hobbies();
+
+        $theme = 'CS:GO';
+        $hobbies->setTopic($theme);
+        $hobbies->addData();
+
+        $theme = 'Camping';
+        $hobbies->setTopic($theme);
+        $hobbies->addData();
+
+        $theme = 'Dogs';
+        $hobbies->setTopic($theme);
+        $hobbies->addData();
+
+        return $this->render('mypage.html.twig',[
+            'hobbies' => $hobbies-> Hobbies()
+        ]);
     }
 }
